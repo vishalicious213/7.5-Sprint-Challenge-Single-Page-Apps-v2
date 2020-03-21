125fdf624fd1c7f8f3432803afe9d6843f9555ad
vishalicious213 <vishalicious213@gmail.com>
2020-03-21 03:01:27 -0400

    styled search. met MVP, but Search screen is repeating character list.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,6 +3,40 @@ import axios from "axios";
 import styled from "styled-components";
 import CharacterCard from "./CharacterCard";
 
+const Input = styled.input`
+  font-size: 1.25rem;
+  border: 3px solid black;
+  border-radius: .5rem;
+  color: darkkhaki;
+  background: cadetblue;
+  margin: 1px;
+  text-shadow:  
+    3px 3px 0 #000,
+    -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;`
+
+const Button = styled.button`
+  font-size: 1.25rem;
+  // width: 10rem;
+  color: cadetblue;
+  font-weight: bold;
+  background: darkkhaki;
+  border: 3px solid black;
+  border-radius: .5rem;
+  margin: 1px;
+  text-shadow:  
+    3px 3px 0 #000,
+    -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+  :hover {
+    color: darkkhaki;
+    background: cadetblue;
+  }`
+
 export default function SearchForm() {
   const [charList, setCharList] = useState([]);
   const [searchString, setSearchString] = useState("");
@@ -28,21 +62,21 @@ export default function SearchForm() {
     <section className="search-form">
      {/* Add a search form here */}
       <form className="search">
-        <input
+        <Input
           type="text"
           onChange={handleSearch}
           value={searchString}
           name="search"
           placeholder="Enter character name"
         />
-        <button>Reset Search</button>
+        <Button>Reset Search</Button>
       </form>
 
       <div className="results">
         {charList.map(char => {
           return (
             // <p>{char.name}</p>
-            <section className="character-list">
+            <section className="character-list" key={char.id}>
               {charList.map(char => {
                 return <CharacterCard key={char.id} name={char.name} species={char.species} type={char.type} status={char.status} gender={char.gender} origin={char.origin.name} location={char.location.name} imgSrc={char.image} />
               })}
