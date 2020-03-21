63fb4594f20c284f4a00369b0e8cf00938d709ed
vishalicious213 <vishalicious213@gmail.com>
2020-03-21 00:42:28 -0400

    used axios to get data for search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function SearchForm() {
+  const [charList, setCharList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      setCharList(response.data.results);
+    })
+    .catch(error => {
+      console.log("Error retrieving data: ", error);
+    })
+  }, []);
  
   return (
     <section className="search-form">
