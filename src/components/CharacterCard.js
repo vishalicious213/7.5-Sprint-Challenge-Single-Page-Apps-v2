c963344bfd52b5484145aedd6d0154329e996a7e
vishalicious213 <vishalicious213@gmail.com>
2020-03-20 21:35:07 -0400

    added styled components and styled character cards to meet part of 2nd MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+        width: 19rem;
+        margin-bottom: 1.5rem;
+        background-color: cadetblue;
+        border-radius: .5rem;
+        padding: .5rem`
 
 export default function CharacterCard(props) {
   // return <span>todo: character</span>;
   return (
-    <div>
+    <Card>
       <img src={props.imgSrc} alt={props.name}></img>
       <h2>{props.name}</h2>
       <p>Species: {props.species}</p>
@@ -12,6 +20,6 @@ export default function CharacterCard(props) {
       <p>Status: {props.status}</p>
       <p>Origin: {props.origin}</p>
       <p>Location: {props.location}</p>
-    </div>
+    </Card>
   )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
@@ -25,7 +25,7 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
       {charList.map(char => {
         // console.log(char.name, char.species, char.status, char.gender)
         return <CharacterCard key={char.id} name={char.name} species={char.species} type={char.type} status={char.status} gender={char.gender} origin={char.origin.name} location={char.location.name} imgSrc={char.image} />
