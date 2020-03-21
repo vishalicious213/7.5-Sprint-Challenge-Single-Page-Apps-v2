cd110e8e66fa658b91897e49fdade824b21504ec
vishalicious213 <vishalicious213@gmail.com>
2020-03-20 21:23:06 -0400

    CharacterCard displays all text data (name, species, type, gender, status, origin, location).

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,6 +5,12 @@ export default function CharacterCard(props) {
   return (
     <div>
       <h2>{props.name}</h2>
+      <p>Species: {props.species}</p>
+      <p>Type: {props.type}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Status: {props.status}</p>
+      <p>Origin: {props.origin}</p>
+      <p>Location: {props.location}</p>
     </div>
   )
 }
